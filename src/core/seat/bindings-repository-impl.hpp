f948e0c86eb15b66b687f5b3aae2143a27493b37
Thiago <killown.matrix@gmail.com>
2026-01-31 04:14:45 -0300

    remove anything tablet

--- src/core/seat/bindings-repository-impl.hpp
+++ src/core/seat/bindings-repository-impl.hpp
@@ -1,48 +1,34 @@
 #pragma once
 
 #include "wayfire/bindings-repository.hpp"
-#include "hotspot-manager.hpp"
 #include "wayfire/signal-definitions.hpp"
+#include <memory>
+#include <vector>
 #include <wayfire/debug.hpp>
 
-struct wf::bindings_repository_t::impl
-{
-    /**
-     * Recreate hotspots.
-     *
-     * The action will take place on the next idle.
-     */
-    void recreate_hotspots()
-    {
-        this->idle_recreate_hotspots.run_once([=] ()
-        {
-            if (enabled > 0)
-            {
-                hotspot_mgr.update_hotspots(activators);
-            } else
-            {
-                hotspot_mgr.update_hotspots({});
-            }
-        });
-    }
-
-    void reparse_extensions();
-
-    binding_container_t<wf::keybinding_t, key_callback> keys;
-    binding_container_t<wf::keybinding_t, axis_callback> axes;
-    binding_container_t<wf::buttonbinding_t, button_callback> buttons;
-    binding_container_t<wf::activatorbinding_t, activator_callback> activators;
-
-    hotspot_manager_t hotspot_mgr;
-
-    wf::signal::connection_t<wf::reload_config_signal> on_config_reload = [=] (wf::reload_config_signal *ev)
-    {
-        recreate_hotspots();
-        reparse_extensions();
-    };
-
-    wf::wl_idle_call idle_recreate_hotspots;
-    wf::wl_idle_call idle_reparse_bindings;
-
-    int enabled = 1;
+namespace wf {
+template <class Option, class Callback> struct binding_t {
+  wf::option_sptr_t<Option> activated_by; // Renamed from 'option' to match .cpp
+  Callback *callback;
+  std::vector<std::any> tags; // Added missing 'tags' member
 };
+
+template <class Option, class Callback>
+using binding_container_t =
+    std::vector<std::unique_ptr<binding_t<Option, Callback>>>;
+
+struct bindings_repository_t::impl {
+  void reparse_extensions();
+
+  binding_container_t<wf::keybinding_t, key_callback> keys;
+  binding_container_t<wf::keybinding_t, axis_callback> axes;
+  binding_container_t<wf::buttonbinding_t, button_callback> buttons;
+  binding_container_t<wf::activatorbinding_t, activator_callback> activators;
+
+  wf::signal::connection_t<wf::reload_config_signal> on_config_reload =
+      [=](wf::reload_config_signal *ev) { reparse_extensions(); };
+
+  wf::wl_idle_call idle_reparse_bindings;
+  int enabled = 1;
+};
+} // namespace wf
--- subprojects/wf-touch/README.md
+++
@@ -1,8 +0,0 @@
-# wf-touch
-
-Touchscreen gesture library
-
-# Acknowledgements
-
-The library's design has been heavily inspired by https://github.com/grahnen/libtouch,
-which has also been used as a reference for some implementation details at times.
